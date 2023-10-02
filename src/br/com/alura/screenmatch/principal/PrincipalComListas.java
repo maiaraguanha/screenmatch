18dc2eb945a63c2904965b2e57d01496e6c8fb85
Maiara <50387178+maiaraguanha@users.noreply.github.com>
2023-10-02 17:06:19 -0300

    collection.sort para realizar ordenação

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,7 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -23,18 +23,37 @@ public class PrincipalComListas {
 
         ArrayList<Titulo> lista = new ArrayList<>();
 
-        lista.add(meuFilme);
+        lista.add(meuFilme);    // add é uma função da Collection de ArrayList.
         lista.add(outroFilme);
         lista.add(filme03);
         lista.add(lost);
 
         for (Titulo item: lista) {
             System.out.println(item.getNome());
-            if (item instanceof Filme filme && filme.getClassificacao() >2 ){
+            if (item instanceof Filme filme && filme.getClassificacao() > 2 ) {
                 System.out.println("Classificação: " + filme.getClassificacao());
             }
-
-
         }
+
+        // Lista criada para aprender a comparar strings
+
+       List<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Raphael");
+        buscaPorArtista.add("Maiara");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);  // função do java.util que coloca em ordem alfabética as string da lista.
+        System.out.println("Depois da ordenação");
+        System.out.println(buscaPorArtista);
+
+        System.out.println("Lista de Titulos ordenados: ");
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano: ");
+        System.out.println(lista);
+
     }
 }
